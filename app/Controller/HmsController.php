d1ef9b142c4aa9b997e8cb81532c21392d9115b6
Abhilash Lohar <abhilashlohar01@gmail.com>
2015-04-11 16:14:22 +0530

    2

--- app/Controller/HmsController.php
+++ app/Controller/HmsController.php
@@ -4867,7 +4867,7 @@ if($this->RequestHandler->isAjax()){
 	}else{
 	$this->layout='session';
 	}
-	echo file_get_contents('http://alerts.sinfini.com/api/v3/web2sms.php?method=sms&api_key=A0cbbf83b328c37dd51dcf5066b7990f3&to=9636653883&sender=NMINVT&message=HELLO&format=json&custom=1,2&flash=0');
+	echo file_get_contents('http://alerts.sinfini.com/api/v3/index.php?method=sms&api_key=A0cbbf83b328c37dd51dcf5066b7990f3&to=9636653883&sender=NMINVT&message=HELLO&format=json&custom=1,2&flash=0');
 	exit;
 	
 
