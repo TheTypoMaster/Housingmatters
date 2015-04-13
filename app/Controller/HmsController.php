281caac69c61037b0a0a1ba9a77e5f437c89e890
rohitkumarjoshi <rohitkumarjoshi43@gmail.com>
2015-04-13 16:13:11 +0530

    set_new_password
    error resolved

--- app/Controller/HmsController.php
+++ app/Controller/HmsController.php
@@ -19742,7 +19742,7 @@ foreach($myArray as $child)
 		{
 		$login_user=$email;	
 
-		 $message_web="<div>
+		echo $message_web="<div>
 		<img src='$ip".$this->webroot."/as/hm/hm-logo.png'/><span  style='float:right; margin:2.2%;'>
 		<span class='test' style='margin-left:5px;'><a href='https://www.facebook.com/HousingMatters.co.in' target='_blank' ><img src='$ip".$this->webroot."/as/hm/fb.png'/></a></span>
 		<a href='#' target='_blank'><img src='$ip".$this->webroot."/as/hm/tw.png'/></a><a href'#'><img src='$ip".$this->webroot."/as/hm/ln.png'/ class='test' style='margin-left:5px;'></a></span>
@@ -19761,7 +19761,7 @@ foreach($myArray as $child)
 		<p>Administrator of $society_name</p><br/>
 		www.housingmatters.co.in
 		</div >
-		</div>";
+		</div>"; 
 
 		}
 
