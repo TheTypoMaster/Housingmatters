7c0849543a80e5eec874ea01ed7169e5a0e5a25a
rohitkumarjoshi <rohitkumarjoshi43@gmail.com>
2015-06-26 11:55:56 +0530

    send alert

--- app/Controller/HmsController.php
+++ app/Controller/HmsController.php
@@ -2901,10 +2901,10 @@ function multiple_flat()
 				$ar[]=array($w,$f);
 			}
 		}	
-				$this->send_alert('<span class="label label-success"><i class="icon-user"></i></span>','you have multiple falt assign',105,$s_user_id,'multiple_flat',array($user_sel));	
+				$this->send_alert('<span class="label label-success"><i class="icon-user"></i></span>','you have multiple falt assign',105,$s_user_id,$this->webroot.'Hms/multiple_flat',array($user_sel));	
 				
 		$this->loadmodel('user');
-		//$this->user->updateAll(array('multiple_flat'=>$ar),array('user_id'=>$user_sel));
+		$this->user->updateAll(array('multiple_flat'=>$ar),array('user_id'=>$user_sel));
 		
 
 		
@@ -3925,16 +3925,8 @@ $this->set('result_alerts',$this->alert->find('all',array('conditions'=>$conditi
 
 function send_alert($icon,$text,$module_id,$element_id,$url,$users)
 {
-$this->layout='blank';
-
-
-
-
-
 $s_society_id=$this->Session->read('society_id');
-
 $now=date('Y-m-d');
-
 $alert_id=$this->autoincrement('alert','alert_id');
 $this->loadmodel('alert');
 $this->alert->saveAll(array('alert_id' => $alert_id,'icon' => $icon,'module_id' => $module_id,'element_id' => $element_id,'text' => $text, 'url' =>$url, 'users' =>$users, 'society_id' =>$s_society_id, 'date' =>$now));
