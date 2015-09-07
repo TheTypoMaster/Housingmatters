0cc8d8e640163bdd583991f5f552bc9b135496e4
nikhilchoubisa <nikhileshvyas@yahoo.com>
2015-09-07 15:29:17 +0530

    sfgfdgd

--- app/Controller/CashbanksController.php
+++ app/Controller/CashbanksController.php
@@ -404,6 +404,8 @@ $wing_id = $collection['user']['wing'];
 $flat_id = (int)$collection['user']['flat'];
 $tenant = (int)$collection['user']['tenant'];
 $user_name = $collection['user']['user_name'];
+$to_mobile = $collection['user']['mobile'];
+$to_email = $collection['user']['email'];
 }
 $wing_flat = $this->requestAction(array('controller' => 'hms', 'action'=>'wing_flat'),array('pass'=>array($wing_id,$flat_id)));									
 }  
@@ -514,7 +516,8 @@ foreach($result_society as $data_society){
 	$sms_is_on_off=(int)@$data_society["society"]["account_sms"];
    }
 //////////////////////////////////////////////////////////////////////////
-$to = "nikhileshvyas4455@gmail.com";
+
+
 if($email_is_on_off==1){
 ////email code//
 
@@ -526,18 +529,16 @@ $sms_allow=(int)$r_sms->sms_allow;
 //$subject="[".$society_name."]- Maintanance bill, ".date('d-M',$bill_start_date)." to ".date('d-M-Y',$bill_end_date)."";
 $subject = "Testing";
 
-$this->send_email($to,'accounts@housingmatters.in','HousingMatters',$subject,$html_receipt,'donotreply@housingmatters.in');
+$this->send_email($to_email,'accounts@housingmatters.in','HousingMatters',$subject,$html_receipt,'donotreply@housingmatters.in');
 
 }
-	
 
-$mobile_number = "9799463210";	
 if($sms_is_on_off==1){
 	if($sms_allow==1){
 $sms="Dear Nikhil ,we have received Rs ".$amount." on ".$sms_date." towards Society Maint. dues. Cheques are subject to realization,".$society_name;
 $sms1=str_replace(' ', '+', $sms);
 
-$payload = file_get_contents('http://alerts.sinfini.com/api/web2sms.php?workingkey='.$working_key.'&sender='.$sms_sender.'&to='.$mobile_number.'&message='.$sms1.''); 
+$payload = file_get_contents('http://alerts.sinfini.com/api/web2sms.php?workingkey='.$working_key.'&sender='.$sms_sender.'&to='.$to_mobile.'&message='.$sms1.''); 
 }
 }	
 //////////////////////////////////////////////////////////////////////////////
@@ -4237,14 +4238,192 @@ foreach($myArray as $child){
 		$multipleRowData = Array( Array("auto_id" => $l, "transaction_date"=> strtotime($TransactionDate), "credit" => $amount,"debit" =>null,"ledger_account_id" => 34, "ledger_sub_account_id" => $account_id,"table_name" => "new_cash_bank","element_id" => $t1, "society_id" => $s_society_id,));
 		$this->ledger->saveAll($multipleRowData);
 			
+//////////////////////////
+		$this->loadmodel('new_cash_bank');
+		$conditions=array("receipt_id" => $k,"society_id"=>$s_society_id);
+		$cursor=$this->new_cash_bank->find('all',array('conditions'=>$conditions));
+		foreach($cursor as $collection)
+		{
+			$receipt_no = (int)$collection['new_cash_bank']['receipt_id'];
+			$d_date = $collection['new_cash_bank']['receipt_date'];
+			$today = date("d-M-Y");
+			$flat_id = $collection['new_cash_bank']['party_name_id'];
+			$amount = $collection['new_cash_bank']['amount'];
+			$society_id = (int)$collection['new_cash_bank']['society_id'];
+			$bill_reference = $collection['new_cash_bank']['reference_utr'];
+			$narration = $collection['new_cash_bank']['narration'];
+			$member = (int)$collection['new_cash_bank']['member_type'];
+			$receiver_name = @$collection['new_cash_bank']['receiver_name'];
+			$receipt_mode = $collection['new_cash_bank']['receipt_mode'];
+			$cheque_number = @$collection['new_cash_bank']['cheque_number'];
+			$which_bank = @$collection['new_cash_bank']['drawn_on_which_bank'];
+			$reference_number = @$collection['new_cash_bank']['reference_number'];
+			$cheque_date = @$collection['new_cash_bank']['cheque_date'];
+			$sub_account = (int)$collection['new_cash_bank']['deposited_bank_id'];
+			$sms_date=date("d-m-Y",($d_date));
 
+	$amount = str_replace( ',', '', $amount );
+	$am_in_words=ucwords($this->requestAction(array('controller' => 'hms', 'action' => 'convert_number_to_words'), array('pass' => array($amount))));
+
+			$this->loadmodel('society');
+			$conditions=array("society_id" => $s_society_id);
+			$cursor2=$this->society->find('all',array('conditions'=>$conditions));
+			foreach ($cursor2 as $collection) 
+			{
+			$society_name = $collection['society']['society_name'];
+			$society_reg_no = $collection['society']['society_reg_num'];
+			$society_address = $collection['society']['society_address'];
+			$sig_title = $collection['society']['sig_title'];
+			}
+				if($member == 2)
+				{
+				$user_name = $receiver_name;
+				$wing_flat = "";
+				}
+				else
+				{
+				$flatt_datta = $this->requestAction(array('controller' => 'hms', 'action' => 'fetch_wing_id_via_flat_id'),array('pass'=>array($flat_id)));
+					foreach ($flatt_datta as $fltt_datttaa) 
+					{
+					$wnngg_idddd = (int)$fltt_datttaa['flat']['wing_id'];
+					}
+
+					$result_lsa = $this->requestAction(array('controller' => 'hms', 'action' => 'fetch_user_info_via_flat_id'),array('pass'=>array($wnngg_idddd,$flat_id)));
+					foreach ($result_lsa as $collection) 
+					{
+					$wing_id = $collection['user']['wing'];  
+					$flat_id = (int)$collection['user']['flat'];
+					$tenant = (int)$collection['user']['tenant'];
+					$user_name = $collection['user']['user_name'];
+					$to_mobile = $collection['user']['mobile'];
+					$to_email = $collection['user']['email'];
+					}
+			$wing_flat = $this->requestAction(array('controller' => 'hms', 'action'=>'wing_flat'),array('pass'=>array($wing_id,$flat_id)));									
+				}  
+			$result2 = $this->requestAction(array('controller' => 'hms', 'action' => 'ledger_sub_account_fetch'),array('pass'=>array($sub_account))); 
+			foreach($result2 as $collection)
+			{
+			$bank_name = $collection['ledger_sub_account']['name'];
+			}
+											
+
+		$date=date("d-m-Y",($d_date));
+
+			$html_receipt='<div style="width:70%;margin:auto;border:solid 1px;background-color:#FFF;" class="bill_on_screen">';
+			$html_receipt.='<div align="center" style="background-color: rgb(0, 141, 210);padding: 5px;font-size: 16px;font-weight: bold;color: #fff;">'.strtoupper($society_name).'</div>
+			<div align="center" style="border-bottom:solid 1px;">
+			<span style="font-size:12px;color:rgb(100, 100, 99);">Regn# '.$society_reg_no.'</span><br/>
+			<span style="font-size:12px;color:rgb(100, 100, 99);">Regn# '.$society_address.'</span>
+			</div>
+			<table width="100%" >
+			<tr>
+			<td>
+				<table width="100%" cellpadding="5px">
+			<tr>
+				<td>Receipt No: '.$receipt_no.'</td>
+				<td align="right">Date: '.$date.'</td>
+			</tr>
+			<tr>
+				<td>
+				Received with thanks from:  <b>'.$user_name.' '.$wing_flat.'</b>
+				<br/>
+				Rupees '.$am_in_words.' Only
+				<br/>';
+				if($receipt_mode=="Cheque"){
+					$html_receipt.='Via '.$receipt_mode.'-'.$cheque_number.' drawn on '.$which_bank.' dated '.$cheque_date;
+				}
+				else{
+					$html_receipt.='Via '.$receipt_mode.'-'.$reference_number.' dated '.$cheque_date;
+				}
+				
+				
+				$html_receipt.='<br/>
+				Payment of previous bill
+				</td>
+				<td></td>
+			</tr>
+		</table>
+		<div style="border-bottom:solid 1px;"></div>
+		<table width="100%" cellpadding="5px">
+			<tr>
+				<td><span style="font-size:16px;"> <b>Rs '.$amount.'</b></span><br/>';
+				if($receipt_mode=="Cheque"){
+					$html_receipt.='Subject to realization of Cheque(s)';
+				}
+				$html_receipt.='</td>
+			</tr>
+		</table>
+		<table width="100%" cellpadding="5px">
+			<tr>
+				<td width="50%"></td>
+				<td align="right">
+				<table width="100%">
+					<tr>
+						<td align="center">
+						For '.$society_name.'
+						</td>
+					</tr>
+				</table>
+				</td>
+			</tr>
+			<tr>
+			<td width="50%"></td>
+			<td align="right">
+			<table width="100%">
+					<tr>
+						<td align="center"><br/>'.$sig_title.'</td>
+					</tr>
+				</table>
+			</td>
+			</tr>
+		</table>
+</td>
+</tr>
+</table>';
+$html_receipt.='</div>';
+
+
+////////////////my Email//////////////
+}
+
+/////////////////////////////////////////////////////////////////////////////
+$this->loadmodel('society');
+$condition=array('society_id'=>$s_society_id);
+$result_society=$this->society->find('all',array('conditions'=>$condition)); 
+$this->set('result_society',$result_society);
+foreach($result_society as $data_society){
+	$society_name=$data_society["society"]["society_name"];
+	$email_is_on_off=(int)@$data_society["society"]["account_email"];
+	$sms_is_on_off=(int)@$data_society["society"]["account_sms"];
+   }
+//////////////////////////////////////////////////////////////////////////
+
+
+if($email_is_on_off==1){
+	$r_sms=$this->hms_sms_ip();
+	$working_key=$r_sms->working_key;
+	$sms_sender=$r_sms->sms_sender; 
+	$sms_allow=(int)$r_sms->sms_allow;
+
+$subject = "Testing";
+
+	$this->send_email($to_email,'accounts@housingmatters.in','HousingMatters',$subject,$html_receipt,'donotreply@housingmatters.in');
+}
+
+if($sms_is_on_off==1){
+if($sms_allow==1){
+$sms="Dear Nikhil ,we have received Rs ".$amount." on ".$sms_date." towards Society Maint. dues. Cheques are subject to realization,".$society_name;
+$sms1=str_replace(' ', '+', $sms);
+
+$payload = file_get_contents('http://alerts.sinfini.com/api/web2sms.php?workingkey='.$working_key.'&sender='.$sms_sender.'&to='.$to_mobile.'&message='.$sms1.''); 
+}
+}	
+			
 	}
 	}
-	
 	
 $output=json_encode(array('report_type'=>'done','text'=>'Please Fill Date in row'));
 die($output);
-  
 }
 ///////////////////////////////// End Save bank Imp ///////////////////////////////////////////////////////////////
 ///////////////////////////// Start bank receipt html view //////////////////////////////////////////////////////////////
